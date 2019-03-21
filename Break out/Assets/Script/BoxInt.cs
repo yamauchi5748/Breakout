69a231c34b1ead9013ee6865f61efc6d96ebeb7c
utiyama5748 <39608214+utiyama5748@users.noreply.github.com>
2019-03-21 20:02:25 +0900

    master

---
+++ Break out/Assets/Script/BoxInt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxInt : MonoBehaviour{
+    
+    public GameObject boxObjPrefab;
+    public GameObject boxesObj;
+    
+    void Awake(){
+        GameObject masterObj = GameObject.Find("Master");
+        for (int x = 0; x < 8; x++) {
+            masterObj.GetComponent<GameMaster>().boxNum++;
+            for (int y = 0; y < 5; y++) {
+                GameObject g = Instantiate(boxObjPrefab, boxesObj.transform);
+                g.transform.position = new Vector3((2f + (1f * y)), 0.4f, (-4.2f + (1.2f * x)));
+                g.GetComponent<Destroyer>().masterObj = masterObj;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
---
+++ Break out/Assets/Script/GameMaster.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour {
+
+    public int boxNum = 0;
+    public float nowTime;
+
+    // Use this for initialization
+    void Start () {
+        nowTime = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        nowTime += Time.deltaTime;
+        if (boxNum <= 0) {
+            GameOver(nowTime.ToString("F0") + "秒でクリアできた！");
+        }
+    }
+
+    public void GameOver(string resultMessage) {
+        DataSender.resultMessage = resultMessage;  //<=====受け取った引数をstatic変数へ格納
+        SceneManager.LoadScene("Result");
+    }
+}
